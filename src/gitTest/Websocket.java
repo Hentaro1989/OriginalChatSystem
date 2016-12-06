20ac05501a0db5cd4ef12d2826e99a14a39c1a72
Hentaro <takethetime1989@gmail.com>
2016-12-06 23:01:18 +0900

    [add]すべてのセッションに対してメッセージを送る機能を追加

--- src/gitTest/Websocket.java
+++ src/gitTest/Websocket.java
@@ -1,17 +1,29 @@
 package gitTest;
 
+import java.io.IOException;
+import java.util.HashSet;
+
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint(value = "/endpoint")
 public class Websocket {
+	private static HashSet<Session> sessions = new HashSet<Session>(); // 全てのセッションを保存するセット
 
 	@OnMessage
-	public String onMessage(String message) {
-		System.out.println("java:" + message);
-		return message;
+	public void onMessage(String message, Session session) throws IOException {
+
+		// シンクロナイズドブロックで同時処理を回避
+		synchronized (message) {
+			sessions.add(session); // セッションを追加
+			for (Session s : sessions) { // すべてのセッションに対しメッセージを送信
+				s.getBasicRemote().sendText(message);
+			}
+		}
+
 	}
 
 	@OnOpen
