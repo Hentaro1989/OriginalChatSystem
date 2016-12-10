a3b4f90bca391e4dd270a14ca8c74f4dc6bc46d3
Hentaro <takethetime1989@gmail.com>
2016-12-10 22:17:04 +0900

    [add]JSONコマンドにより（name,chat）行う処理を分岐 ※チャットを入力した人の名前が画面に出力される

--- src/gitTest/Websocket.java
+++ src/gitTest/Websocket.java
@@ -16,27 +16,28 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @ServerEndpoint(value = "/endpoint")
 public class Websocket {
 	private static HashSet<Session> sessions = new HashSet<Session>(); // 全てのセッションを保存するセット
+	private static HashMap<Session, String> membersName = new HashMap<Session, String>(); //セッションごとにユーザの名前を管理
 
 	@OnMessage
-	synchronized public void onMessage(String message) throws IOException {
+	synchronized public void onMessage(String message, Session session) throws IOException {
 
-		HashMap<String, Object> json = new ObjectMapper().readValue(message,HashMap.class);
+		HashMap<String, Object> json = new ObjectMapper().readValue(message, HashMap.class);
 		String cmd = (String) json.get("cmd");
 
-		//JSONコマンドが「name」の時の処理
-		if(cmd.equals("name")){
-
+		// JSONコマンドが「name」の時の処理
+		if (cmd.equals("name")) {
+			membersName.put(session, (String) json.get("name")); //セッションと入力された名前をマッピング
+			return;
 		}
 
-		//JSONコマンドが「chat」の時の処理
-		if(cmd.equals("chat")){
-
-		}
-
-
-		for (Session s : sessions) { // すべてのセッションに対しメッセージを送信
-			System.out.println(message);
-			s.getBasicRemote().sendText(message);
+		// JSONコマンドが「chat」の時の処理
+		if (cmd.equals("chat")) {
+			String yourName = membersName.get(session); //セッションに紐づいている名前を取得
+			message = (String) json.get("text"); //JSONコマンド「text」に入力されて文字列を取得
+			for (Session s : sessions) { // すべてのセッションに対しメッセージを送信
+				s.getBasicRemote().sendText(yourName + "さん ： " + message);
+			}
+			return;
 		}
 
 	}
@@ -45,22 +46,21 @@ public class Websocket {
 	synchronized public void onOpen(Session session) {
 		synchronized (session) {
 			sessions.add(session); // セッションを追加
-			System.out.println("【Method】onOpen()");
-			System.out.println("【current sessions】" + sessions.size());
+			System.out.println("【Method】onOpen()  " + "【current sessions】" + sessions.size());
 			System.out.println();
 		}
 	}
 
 	@OnClose
 	synchronized public void onClose(Session session) {
+		membersName.remove(session); //セッションから外れた人の名前を削除
 		sessions.remove(session); // セッションを破棄
-		System.out.println("【Method】onClose()");
-		System.out.println("【current sessions】" + sessions.size());
+		System.out.println("【Method】onClose()  " + "【current sessions】" + sessions.size());
 		System.out.println();
 	}
 
 	@OnError
-	synchronized public void onError(Throwable throwable){
+	synchronized public void onError(Throwable throwable) {
 		System.out.println(throwable.getCause());
 		System.out.println();
 	}
