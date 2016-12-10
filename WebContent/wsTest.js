21c3b3e7d18665942f13a7b457a33e1f224f5d40
Hentaro <takethetime1989@gmail.com>
2016-12-10 22:15:24 +0900

    [add]宅内linuxサーバテスト用のwebsocketを追加

--- WebContent/wsTest.js
+++ WebContent/wsTest.js
@@ -1,5 +1,6 @@
 $(function() {
 
+	//var uri = "ws://192.168.11.10:8080/gitTest/endpoint"; //宅内Linuxサーバー用
 	var uri = "ws://localhost:8080/gitTest/endpoint"; //接続先URI
 	var ws = new WebSocket(uri); // Websocketオブジェクト
 
